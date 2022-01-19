3719121884d9d4a8a48f63b842ca6c6a309bafe6
Lizzie Magpayo <lizziem@canva.com>
2022-01-19 11:03:15 +1100

    add signature and timestamp verification

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createHmac } = require("crypto");
 
 const app = express();
 
@@ -14,11 +15,21 @@ const auth = new google.auth.JWT(
 );
 const drive = google.drive({ version: "v3", auth });
 
-app.use(express.json());
+app.use(
+    express.json({
+      verify: (request, response, buffer) => {
+        request.rawBody = buffer.toString();
+      },
+    })
+);
 
 app.post('/content/resources/find', listPhotosFromDrive);
 
 function listPhotosFromDrive(request, response) {
+  if (!isValidPostRequest(process.env.CLIENT_SECRET, request)) {
+    response.sendStatus(401);
+    return;
+  }
 
   const nextPageToken = request.body.continuation || '';
 
@@ -64,8 +75,73 @@ function listPhotosFromDrive(request, response) {
   })
 }
 
+const isValidPostRequest = (secret, request) => {
+  // Verify the timestamp
+  const sentAtSeconds = request.header("X-Canva-Timestamp");
+  const receivedAtSeconds = new Date().getTime() / 1000;
+
+  if (!isValidTimestamp(sentAtSeconds, receivedAtSeconds)) {
+    console.log('wow invalid timestamp')
+    return false;
+  }
+
+  // Construct the message
+  const version = "v1";
+  const timestamp = request.header("X-Canva-Timestamp");
+  const path = getPathForSignatureVerification(request.path);
+  const body = request.rawBody;
+  const message = `${version}:${timestamp}:${path}:${body}`;
+
+  // Calculate a signature
+  const signature = calculateSignature(secret, message);
+
+  // Reject requests with invalid signatures
+  if (!request.header("X-Canva-Signatures").includes(signature)) {
+    console.log('wow invalid signature')
+    return false;
+  }
+
+  return true;
+};
+
+const isValidTimestamp = (
+    sentAtSeconds,
+    receivedAtSeconds,
+    leniencyInSeconds = 300
+) => {
+  return (
+      Math.abs(Number(sentAtSeconds) - Number(receivedAtSeconds)) <
+      Number(leniencyInSeconds)
+  );
+};
+
+const getPathForSignatureVerification = (input) => {
+  const paths = [
+    "/configuration",
+    "/configuration/delete",
+    "/content/resources/find",
+    "/editing/image/process",
+    "/editing/image/process/get",
+    "/publish/resources/find",
+    "/publish/resources/get",
+    "/publish/resources/upload",
+  ];
+
+  return paths.find((path) => input.endsWith(path));
+};
+
+const calculateSignature = (secret, message) => {
+  // Decode the client secret
+  const key = Buffer.from(secret, "base64");
+
+  // Calculate the signature
+  return createHmac("sha256", key).update(message).digest("hex");
+};
+
+
 function fromProxy(url) {
   return 'https://res.cloudinary.com/duw3ba6fo/image/fetch/' + encodeURIComponent(url.replace('&export=download', ''));
 }
+
 
 app.listen(process.env.PORT || 3000);
